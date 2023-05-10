c17935c228cee0b1d4e81ed779b5e5ca2af760e8
siondawson <>
2023-05-10 18:42:07 +0000

    add code o increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,8 +51,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -76,11 +78,23 @@ function calculateCorrectAnswer() {
 
     }
 }
+/***
+ * Get the current score from the DOM and increments by 1
+ */
 function incrementScore() {
 
-}
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
+
+}
+/***
+ * Get the current tally of incorrect answers from the DOM and increments by 1
+*/
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
